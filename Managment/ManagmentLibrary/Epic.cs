7fcffb9e4d0696a7a62d8ec6ca2a07e0c7c207b3
207AndrewLosyukov <71185461+207AndrewLosyukov@users.noreply.github.com>
2021-10-02 18:13:54 +0300

    Add files via upload

---
+++ Managment/ManagmentLibrary/Epic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagmentLibrary
+{
+    [Serializable]
+    public class Epic : Tasks
+    {
+        // Лист задач, вложенных в Epic.
+        public List<Tasks> epicTask = new List<Tasks>();
+
+        /// <summary>
+        /// Беспараметрический конструктор для сериализации.
+        /// </summary>
+        public Epic()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор с ссылкой на базовый.
+        /// </summary>
+        /// <param name="status">Статус</param>
+        /// <param name="name">Имя задачи</param>
+        public Epic(string status, string name) : base(status, name)
+        {
+        }
+        /// <summary>
+        /// Сокрытый метод для добавления исполнителя в задачу.
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public new static void Append(Tasks task)
+        {
+            Console.WriteLine("У задачи этого типа не может быть исполнителей, т. к. она включает в себя подзадачи");
+        }
+        /// <summary>
+        /// Переопределенный метод для вывода задачи Epic.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string result = $"{name} {GetType().ToString().Split('.')[1]} {status} {date}";
+            if (epicTask.Count != 0)
+            {
+                result += "\nСуществуют следующие подзадачи:";
+                foreach (Tasks task in epicTask)
+                {
+                    result += $"\n\t{task.GetType().ToString().Split('.')[1]} {task.name}";
+                }
+            }
+            else
+            {
+                result += "\n\tПодзадач нет";
+            }
+            return result;
+        }
+    }
+}
---
+++ Managment/ManagmentLibrary/Serialized.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagmentLibrary
+{
+    /// <summary>
+    /// Сериализация списка исполнителей.
+    /// </summary>
+    public class Users
+    {
+        public List<User> users;
+    }
+    
+    /// <summary>
+    /// Сериализация списка проектов.
+    /// </summary>
+    public class Projects
+    {
+        public List<Project> projects;
+    }
+}
