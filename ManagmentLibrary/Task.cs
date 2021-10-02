2c1db982515606e2d758611fc2ff71ce53f327a6
207AndrewLosyukov <71185461+207AndrewLosyukov@users.noreply.github.com>
2021-10-02 18:09:55 +0300

    Add files via upload

---
+++ ManagmentLibrary/Task.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagmentLibrary
+{
+    [Serializable]
+    public class Task : Tasks, IAssignable
+    {
+        // Список исполнителей.
+        public List<User> users = new List<User>();
+
+        /// <summary>
+        /// Беспараметрический конструктор для сериализации.
+        /// </summary>
+        public Task()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор с ссылкой на базовый.
+        /// </summary>
+        /// <param name="status">Статус</param>
+        /// <param name="name">Имя задачи</param>
+        public Task(string status, string name) : base(status, name)
+        {
+        }
+
+        /// <summary>
+        /// Сокрытие метода добавления исполнителя.
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public new static void Append(Tasks task)
+        {
+            try
+            {
+                // Добавление исполнителя, только если его еще нет.
+                if (task.Users.Count == 0)
+                {
+                    Console.WriteLine("Введите имя исполнителя из этого списка:");
+                    // Вывод списка исполнителей.
+                    User.GetUsers();
+                    string name = Console.ReadLine();
+                    // Проверка на существование заданного исполнителя.
+                    if (!Equals(User.users.Find(x => x.username == name), null))
+                    {
+                        // Добавление исполнителя.
+                        task.Users.Add(User.users.Find(x => x.username == name));
+                        Console.WriteLine("Исполнитель успешно добавлен");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не найдено такого исполнителя");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("У задачи этого типа может быть максимум один исполнитель");
+                }
+            }
+            // Отлов исключений.
+            catch (Exception)
+            {
+                Console.WriteLine("Некорректные данные");
+            }
+        }
+    }
+}
